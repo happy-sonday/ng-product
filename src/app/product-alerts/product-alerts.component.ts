62014ae3a1943b8523699cf27173f1d60908126f
sws <sws@paywith.co.kr>
2021-02-14 15:34:57 +0900

    자식 컴포넌트에서 output 데코레이터를 이용해  부모컴포넌트로  이벤트 바인딩

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Input } from '@angular/core';
+import { EventEmitter, Input, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,6 +10,8 @@ export class ProductAlertsComponent implements OnInit {
   /** ProductListComponent 로부터 값을 전달받음 */
   @Input() product;
 
+  /** notify property가 변경될때 list component로 이벤트를 발생할 수 있도록 함 */
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,6 +12,14 @@ export class ProductListComponent implements OnInit {
   share() {
     alert('The product has been shared!');
   }
+
+  /** 자식 컴포넌트 product-alerts에서
+   *  Output 데코레이터로 정의된 notify에 의해 이벤트가 발생할때
+   *  action 정의
+   */
+  onNotify() {
+    alert('product-alerts에 event를 전달하여 부모 이벤트에서 alert 발생');
+  }
   constructor() {}
 
   ngOnInit(): void {}
